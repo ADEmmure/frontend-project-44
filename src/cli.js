28d1682e18ee122491cf948b9a83dfbbc6874f01
dsaristov <aristov.dmitriy@hexlet.ru>
2023-03-03 15:35:20 +0300

    add CodeClimate and Eslint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const getWelcome = () => {
-const name = readlineSync.question('May I have your name? ');
-console.log('Welcome to the Brain Games!');
-console.log('Hello, ' + name + '!');
-}
+  const name = readlineSync.question('May I have your name? ');
+  console.log('Welcome to the Brain Games!');
+  console.log(`Hello, ${name}!`);
+};
 export default getWelcome;
