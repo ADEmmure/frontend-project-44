4a500ef4b22a91483ccf470867326ad3e54c1359
dsaristov <aristov.dmitriy@hexlet.ru>
2023-03-21 18:33:35 +0300

    patch brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,38 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import getRandomNumber from '../src/getRandomNumber.js';
+import runBrainCalc from '../src/games/calc.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('What is the result of the expression?');
-let i = 1;
-while (i <= 3) {
-  const randomNumber1 = getRandomNumber(10, 20);
-  const randomNumber2 = getRandomNumber(0, 10);
-  const randomSymbol = getRandomNumber(0, 3); // 1 = '+', 2 = '-', 3 = '*'
-  let answer = 0;
-  let question = '';
-  if (randomSymbol === 1) {
-    question = `${randomNumber1} + ${randomNumber2}`;
-    answer = (randomNumber1 + randomNumber2);
-  } else if (randomSymbol === 2) {
-    question = `${randomNumber1} - ${randomNumber2}`;
-    answer = (randomNumber1 - randomNumber2);
-  } else if (randomSymbol === 3) {
-    question = `${randomNumber1} * ${randomNumber2}`;
-    answer = (randomNumber1 * randomNumber2);
-  }
-  const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
-  if (userAnswer === String(answer)) {
-    console.log('Correct!');
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
-    i += 1;
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'\nLet's try again, ${name}!`);
-    break;
-  }
-}
+runBrainCalc();
---
+++ src/games/calc.js
@@ -0,0 +1,33 @@
+import getRandomNumber from '../getRandomNumber.js';
+import runGameLogic, { roundsNumber } from '../index.js';
+
+const descriptions = 'What is the result of the expression?';
+
+const generateRound = () => {
+  const firstNumber = getRandomNumber(10, 20);
+  const secondNumber = getRandomNumber(0, 10);
+  const randomSymbol = getRandomNumber(0, 3); // 1 = '+', 2 = '-', 3 = '*'
+  let correctAnswer = '';
+  let question = '';
+  if (randomSymbol === 1) {
+    question = `${firstNumber} + ${secondNumber}`;
+    correctAnswer = String(firstNumber + secondNumber);
+  } else if (randomSymbol === 2) {
+    question = `${firstNumber} - ${secondNumber}`;
+    correctAnswer = String(firstNumber - secondNumber);
+  } else if (randomSymbol === 3) {
+    question = `${firstNumber} * ${secondNumber}`;
+    correctAnswer = String(firstNumber * secondNumber);
+  }
+  return [question, correctAnswer];
+};
+
+const runBrainCalc = () => {
+  const rounds = [];
+  for (let i = 0; i < roundsNumber; i += 1) {
+    rounds[i] = generateRound();
+  }
+  return runGameLogic(rounds, descriptions);
+};
+
+export default runBrainCalc;
